80567ac1081e22775d91d0fca53d23f8127d13d5
Lyndsey Herbert <lhe@amp.co>
2020-01-17 11:51:51 +0000

    UPDATED sample data

--- helpers/config.js
+++ helpers/config.js
@@ -10,17 +10,4 @@ export const tempPath = path.join(projectPath, 'var/view_preprocessed/frontools/
 export const themes = configLoader('themes.json', false)
 export const browserSyncInstances = {}
 
-// export const gulpicon = {
-//   'datasvgcss': '_components.icons-data-svg.scss',
-//   'datapngcss': '_components.icons-data-png.scss',
-//   'urlpngcss': '_components.icons-url-png.scss',
-//   'pngfolder': '../../images/icons/png',
-//   'cssprefix': '.c-icon--',
-//   'defaultWidth': '28px',
-//   'defaultHeight': '28px',
-//   'compressPNG': true,
-//   'enhanceSVG': true,
-//   'themeSrc': 'images/icons/svg',
-//   'themeDest': 'web',
-//   'themeTemplate': '/images/icons/templates/default-css.hbs',
-// }
+export const svgConfig = configLoader('svg-sprite.json')
--- helpers/svg.js
+++ helpers/svg.js
@@ -7,58 +7,21 @@ import logger from 'gulp-logger'
 import plumber from 'gulp-plumber'
 import notify from 'gulp-notify'
 import svgSprite from 'gulp-svg-sprite'
-import configLoader from './config-loader'
-import { env, tempPath, projectPath, themes, browserSyncInstances } from './config'
-
-const gulpicon = {
-  'datasvgcss': '_components.icons-data-svg.scss',
-  'datapngcss': '_components.icons-data-png.scss',
-  'urlpngcss': '_components.icons-url-png.scss',
-  'pngfolder': '../../images/icons/png',
-  'cssprefix': '.c-icon--',
-  'defaultWidth': '28px',
-  'defaultHeight': '28px',
-  'compressPNG': true,
-  'enhanceSVG': true,
-  'themeSrc': 'images/icons/svg',
-  'themeDest': 'web',
-  'themeTemplate': '/images/icons/templates/default-css.hbs',
-}
+import { env, projectPath, themes, browserSyncInstances } from './config'
 
 export default name => {
   const theme = themes[name]
   const srcBase = path.join(projectPath, theme.src)
-  const svgDest = path.join(srcBase, gulpicon.themeSrc)
   const dest = path.join(srcBase, gulpicon.themeDest)
-  // const svgConfig = configLoader('svg-sprite.json')
-  const templatePath = path.join(srcBase, gulpicon.themeTemplate)
+
 
   const config = {
+    ...svgConfig,
     shape: {
-      dest: gulpicon.themeSrc,
       id: {
         generator: file => path.basename(file, '.svg')
       }
     },
-    svg: {
-      xmlDeclaration: false,
-      doctypeDeclaration: false
-    },
-    mode: {
-      css: {
-        dest: 'scss/components',
-        prefix: '.c-icon--%s',
-        common: 'c-icon',
-        dimensions: true,
-        bust: false,
-        example: false,
-        render: {
-          scss: {
-            dest: '_components.icons.scss'
-          }
-        }
-      }
-    }
   }
 
   const gulpTask = src(`${srcBase}/**/icons/**/*.svg`)
